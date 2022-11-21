3680baaf5605d32270f5dfb95bed831457757685
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-11-21 16:20:34 -0500

    Update third.js

--- data/third.js
+++ data/third.js
@@ -1,4 +1,4 @@
-const third = 
+const seventies = 
 [
   {
     "Plane": "Lockheed L-1011 TriStar",
@@ -16,6 +16,8 @@ const third =
         "Range": "9,899 km",
         "Top Speed": "2,370 km/h",
         "Engine type": "Rolls-Royce RB211",
+        "Passengers":"256",
+        "Flight Crew":"3",
         "Manufacturer(s)": "Lockheed",
         "Image": "https://en.wikipedia.org/wiki/File:11df_-_Delta_Air_Lines_Lockheed_L-1011_TriStar_1;_N1732D@FLL;30.01.1998_(5016025629).jpg",
         "Countries used": [
@@ -42,6 +44,8 @@ const third =
         "Range": "5,000 km",
         "Top Speed": "831–871 km/h",
         "Engine type": "Kuznetsov NK-86",
+        "Passengers":"350",
+        "Flight Crew":"3-4",
         "Manufacturer(s)": "Voronezh Aircraft Production Association",
         "Image": "https://imgs.search.brave.com/l0LVgNIMAiLtgTg-xJWLieHjXoCaB0vG8G_JO8NM2kg/rs:fit:713:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC53/UFdHd1gyY0Rhc21O/VExUbXkySXRRSGFF/NyZwaWQ9QXBp",
         "Countries used": [
@@ -73,6 +77,8 @@ const third =
         "Range": "14,320 km",
         "Top Speed": "939 km/h",
         "Engine type": "Pratt & Whitney JT9D, Rolls-Royce RB211, General Electric CF6",
+        "Passengers":"467",
+        "Flight Crew":"3",
         "Manufacturer(s)": "Mitsubishi Heavy Industries, Ltd., Mitsubishi Aircraft Company",
         "image": "https://en.wikipedia.org/wiki/File:B-747_Iberia.jpg",
         "Countries used": [
@@ -85,218 +91,73 @@ const third =
     ]
   },
   {
-    "Plane": "Shenyang J-8",
+    "Plane": "McDonnell Douglas DC-10",
     "results": [
       {
-        "Definition": "High-speed, high-altitude Chinese-built single-seat interceptor fighter aircraft",
-        "Introduction": "1980",
-        "National Origin": "China",
-        "First flight": "July 5, 1969",
-        "Retired": "N/A",
-        "Weight": "15,288 kg",
-        "Length": "21.6 m",
-        "Wingspan": "9.3 m",
-        "Units": ">408",
-        "Range": "3,300 km",
-        "Top Speed": "2,339 km/h",
-        "Engine type": "Guizhou WP-13B",
-        "Manufacturer(s)": "Shenyang Aircraft Corporation",
-        "Image": "https://imgs.search.brave.com/jKLhkEGH1A8kWjTd8d-YNVzDcadH08-LBlKjl9bV1AQ/rs:fit:702:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4x/YzdFTlo5ZkF1WEJq/LUx0MnpSSXZBSGFG/QSZwaWQ9QXBp",
-        "Countries used": [
-        "China"
-      ],
-        
-      },
-    ]
-  },
-  {
-    "Plane": "Sukhoi Su-15",
-    "results": [
-      {
-        "Definition": "A twin-engined supersonic interceptor",
-        "Introduction": "1965",
-        "National Origin": "Soviet Union",
-        "First flight": "30 May 1962 ",
-        "Retired": "1996 (Ukraine)",
-        "Weight": "10,760 kg",
-        "Length": "21.6 m",
-        "Wingspan": "9.4 m",
-        "Units": "1,290",
-        "Range": "1,050 km",
-        "Top Speed": "3,063 km/h",
-        "Engine type": "Tumansky R-13F-300",
-        "Manufacturer(s)": "Sukhoi",
-        "Image": "https://imgs.search.brave.com/mJXU-jrK8SKv2E_gS23J2luvOWMOM1p0HoFojMakwTw/rs:fit:724:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC4t/VWlFRHYyRUhrMW1N/QWNwN0N1RXZBSGFF/MiZwaWQ9QXBp",
-        "Countries used": [
-        "Soviet Union/Russia",
-        "Ukraine"
-      ],
-        
-      },
-    ]
-  },
-  {
-    "Plane": "Convair F-106 Delta Dart",
-    "results": [
-      {
-        "Definition": "Primary all-weather interceptor aircraft",
-        "Introduction": "1959",
+        "Definition": "American trijet wide-body aircraft",
+        "Introduction": "August 5, 1971",
         "National Origin": "U.S.A.",
-        "First flight": "26 December 1956",
-        "Retired": "August 1986",
-        "Weight": "23,760 lbs",
-        "Length": "22 m",
-        "Wingspan": "12 m",
-        "Units": "1,290",
-        "Range": "2,897 km",
-        "Top Speed": "2,455 km/h",
-        "Engine type": "Pratt & Whitney J75-P-17",
-        "Manufacturer(s)": "Convair",
-        "Image": "https://amcmuseum.org/wp-content/uploads/2014/10/F-106-59-0023-177-FIG-NJ-ANG-1985.png",
-        "Countries used": [
-         "United States"
-      ],
-        
-      },
-    ]
-  },
-  {
-    "Plane": "VFW VAK 191B",
-    "results": [
-      {
-        "Definition": "German VTOL nuclear strike fighter",
-        "Introduction": "1969",
-        "National Origin": "Germany",
-        "First flight": "20 September 1971",
-        "Retired": "March 2011",
-        "Weight": "5,562 kg",
-        "Length": "16.4 m",
-        "Wingspan": "6.16 m",
-        "Units": "3",
-        "Range": "400 km",
-        "Top Speed": "1,108 km/h",
-        "Engine type": "Rolls-Royce RB.162, Rolls-Royce/MAN Turbo RB.193",
-        "Manufacturer(s)": "Vereinigte Flugtechnische Werke",
-        "Image": "https://imgs.search.brave.com/NTcVRs6LPLICU4bGnRUeVoDoK7iGZ-2dTA-MC1tuepM/rs:fit:711:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5k/OHhDeWpxNzd3SUlR/X2xkcVZFem1nSGFF/OCZwaWQ9QXBp",
-        "Countries used": [
-        "Germany",
- 
-      ],
-        
-      },
-    ]
-  },
-   {
-    "Plane": "Saab 37 Viggen",
-    "results": [
-      {
-        "Definition": "Swedish single-seat, single-engine, short-medium range fighter and attack aircraft",
-        "Introduction": "1971",
-        "National Origin": "Sweden",
-        "First flight": "8 February 1967",
-        "Retired": "25 November 2005",
-        "Weight": "9,500 kg",
-        "Length": "16.4 m",
-        "Wingspan": "10.6 m",
-        "Units": "329",
-        "Range": "2,000 km",
-        "Top Speed": "1,108 km/h",
-        "Engine type": "Volvo RM8",
-        "Manufacturer(s)": "Saab",
-        "Image": "https://imgs.search.brave.com/Pu5SUXTnFYTtwjbWR-iT3_QSBqltW6OcvQn-gT5vfRw/rs:fit:844:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5V/TE1TNU83UzljT2FZ/dFRqN1RhalF3SGFF/SyZwaWQ9QXBp",
-        "Countries used": [
-        "Sweden",
- 
-      ],
-        
-      },
-    ]
-  },
-  {
-    "Plane": "Northrop F-5",
-    "results": [
-      {
-        "Definition": "Swedish single-seat, single-engine, short-medium range fighter and attack aircraft",
-        "Introduction": "1971",
-        "National Origin": "Sweden",
-        "First flight": "8 February 1967",
-        "Retired": "25 November 2005",
-        "Weight": "9,500 kg",
-        "Length": "16.4 m",
-        "Wingspan": "10.6 m",
-        "Units": "329",
-        "Range": "2,000 km",
-        "Top Speed": "1,108 km/h",
-        "Engine type": "Volvo RM8",
-        "Manufacturer(s)": "Saab",
-        "Image": "https://imgs.search.brave.com/mZXXE1ztdjYhVkYZ5ei69Enu7y19DK-2hLvduRuASCw/rs:fit:702:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC40/eGZiaVJoVGdCVWdT/dFNfT3E4SzBBSGFG/QSZwaWQ9QXBp",
-        "Countries used": [
-        "Sweden",
- 
-      ],
-        
-      },
-    ]
-  },
-  
-  {
-    "Plane": "Mikoyan-Gurevich MiG-25",
-    "results": [
-      {
-        "Definition": "Supersonic interceptor and reconnaissance aircraft",
-        "Introduction": "1970",
-        "National Origin": "Soviet Union",
-        "First flight": "6 March 1964",
+        "First flight": "August 29, 1970",
         "Retired": "N/A",
-        "Weight": "20,000 kg",
-        "Length": "19.75 m",
-        "Wingspan": "14.01 m",
-        "Units": "1,186",
-        "Range": "1,730 km",
-        "Top Speed": "1,700 km/h",
-        "Engine type": "Tumansky R-15B-300",
-        "Manufacturer(s)": "Northrop Corporation",
-        "Image": "https://imgs.search.brave.com/Yaw8GBgCT561yK6IqdPfisupcCHoOYNz5YvX5TlPbdI/rs:fit:713:225:1/g:ce/aHR0cHM6Ly90c2Uy/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC56/MWVZYzY0SVdaTDY2/bjhMcTFOa2hBSGFF/NyZwaWQ9QXBp",
+        "Weight": "122,567 kg",
+        "Length": "55.54 m",
+        "Wingspan": "50.39 m",
+        "Units": "386",
+        "Range": "9,400 km",
+        "Top Speed": "940 km/h",
+        "Engine type": "Guizhou WP-13B",
+        "Passengers":"399",
+        "Flight Crew":"3",
+        "Manufacturer(s)": "McDonnell Douglas",
+        "Image": "https://en.wikipedia.org/wiki/File:Continental_Airlines_DC-10.jpg",
         "Countries used": [
-        "Algeria",
-        "Armenia",
-        "Russia",
-        "Syria",
-        "Turkmenistan",
- 
+        "U.S.A.",
+        "Bangladesh",
+        "Netherlands"
       ],
         
       },
     ]
   },
-  
   {
-    "Plane": "British Aerospace Harrier II",
+    "Plane": "Airbus A300",
     "results": [
       {
-        "Definition": "Swedish single-seat, single-engine, short-medium range fighter and attack aircraft",
-        "Introduction": "1989",
-        "National Origin": "U.K./U.S.A.",
-        "First flight": "30 April 1985",
-        "Retired": "March 2011 ",
-        "Weight": "5,700 kg",
-        "Length": "14.12 m",
-        "Wingspan": "9.25 m",
-        "Units": "1,204",
-        "Range": "1,667 km",
-        "Top Speed": "1,065 km/h",
-        "Engine type": "Rolls-Royce Pegasus Mk. 105",
-        "Manufacturer(s)": "British Aerospace/McDonnell Douglas/BAE Systems/Boeing",
-        "Image": "https://imgs.search.brave.com/a0g2d3rC0BxivCcPhRwi-m0-bHEOcEsI_bBa5sQSC0o/rs:fit:1200:1080:1/g:ce/aHR0cHM6Ly9pbWFn/ZXM1LmFscGhhY29k/ZXJzLmNvbS85NzUv/dGh1bWItMTkyMC05/NzU5MDYuanBn",
+        "Definition": "Wide-body airliner developed and manufactured by Airbus",
+        "Introduction": "23 May 1974 (Air France)",
+        "National Origin": "Multinational",
+        "First flight": "28 October 1972",
+        "Retired": "N/A",
+        "Weight": "81,707 kg",
+        "Length": "54.08 m",
+        "Wingspan": "44.84 m",
+        "Units": "561",
+        "Range": "7,500 km",
+        "Top Speed": "833 km/h",
+        "Engine type": "CF6-80C2/PW4158",
+        "Passengers":"345",
+        "Flight Crew":"2-3",
+        "Manufacturer(s)": "McDonnell Douglas",
+        "Image": "https://en.wikipedia.org/wiki/File:Airbus_A300B4-603,_Lufthansa_AN0520385.jpg",
         "Countries used": [
-        "U.K.",
-       
- 
+        "U.S.A.",
+        "France",
+        "South Korea",
+        "Saudi Arabia",
+        "Switzerland",
+        "Taiwan",
+        "Germany",
+        "China",
+        "India",
+        "Thailand",
+        "Kuwait",
+        "Iran",
       ],
         
       },
     ]
   },
+
 ]
 
-module.exports = third;
+module.exports = seventies;
