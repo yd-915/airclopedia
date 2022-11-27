5388f559f68d47cd0519f0355dc5f0970f52fa7c
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-11-26 20:10:48 -0500

    Create sixth.js

---
+++ data/sixth.js
@@ -0,0 +1,39 @@
+const twothousands = 
+[
+  {
+    "Plane": "McDonnell Douglas MD-90",
+    "Results": [
+      {
+        "Definition": "American five-abreast single-aisle airliner",
+        "Introduction": "1995",
+        "National Origin": "U.S.A.",
+        "First flight": "February 22, 1993",
+        "Retired": "June 2, 2020",
+        "Range": "4,143 km",
+        "Top Speed": "812 km/h",
+        "Weight": "40,098 kg",
+        "Lenght": "46.51 m",
+        "Wingspan": "32.86 m",
+        "Units": "116",
+        "Engine type": "International Aero Engines V2500",
+        "Passengers": "163-172",
+        "Manufacturer(s)": "McDonnell Douglas",
+        "Image": "https://en.wikipedia.org/wiki/File:Delta_McDonnell_Douglas_MD-90-30_N908DA_(2933350256).jpg",
+        "Countries used": [
+          "U.S.A.",
+          "Indonesia",
+          "Saudi Arabia",
+          "Taiwan",
+          "China",
+          "Japan",
+          
+      
+      ],
+        
+      },
+    ]
+  },
+
+]
+
+module.exports = twothousands;
