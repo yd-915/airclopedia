25fd091741e2bb24b065c70db6b9cd3257b88246
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-11-26 22:24:28 -0500

    Update sixth.js

--- data/sixth.js
+++ data/sixth.js
@@ -33,6 +33,39 @@ const twothousands =
       },
     ]
   },
+ {
+    "Plane": "Antonov An-148",
+    "Results": [
+      {
+        "Definition": "Ukrainian regional jet",
+        "Introduction": "2 June 2009",
+        "National Origin": "Ukraine",
+        "First flight": "17 December 2004",
+        "Retired": "June 2, 2020",
+        "Range": "2,500 km",
+        "Top Speed": "870 km/h",
+        "Weight": "49,000 kg",
+        "Lenght": "30.83 m",
+        "Wingspan": "29 m",
+        "Units": "47",
+        "Engine type": "Progress D-436",
+        "Passengers": "86–99",
+        "Manufacturer(s)": "McDonnell Douglas",
+        "Image": "https://en.wikipedia.org/wiki/File:Delta_McDonnell_Douglas_MD-90-30_N908DA_(2933350256).jpg",
+        "Countries used": [
+          "Ukraine",
+          "Russia",
+          "North Korea",
+          "Cuba",
+          "China",
+          "Japan",
+          
+      
+      ],
+        
+      },
+    ]
+  },
 
 ]
 
